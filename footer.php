f6cf2d4546714ebf3df9e0a8ca770515a534ec89
Johannes Jakob <hi@johjakob.com>
2021-06-09 15:42:21 +0200

    Layout: Update website footer

--- footer.php
+++ footer.php
@@ -12,18 +12,12 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+		<div class="site-organizer">
+			<p>veranstaltet durch</p>
+			
+		</div>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'klink' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'klink' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'klink' ), 'klink', '<a href="https://johjakob.com">Johannes Jakob</a>' );
-				?>
+			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
