5b971cf2a30af5c5915e8df22227dc586058c91a
Johannes Jakob <hi@johjakob.com>
2021-06-12 21:45:46 +0200

    Footer: Update footer
    - Adds footer menu
    - Updates footer styles

--- footer.php
+++ footer.php
@@ -17,12 +17,12 @@
 			<a href="http://dessau-vorort.de" class="site-organizer-logo" style="mask-image: url('<?php echo get_theme_file_uri('assets/images/vorort_logo.svg'); ?>'); -webkit-mask-image: url('<?php echo get_theme_file_uri('assets/images/vorort_logo.svg'); ?>');"></a>
 		</div>
 		<div class="site-info">
-			
-		</div><!-- .site-info -->
+			<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+		</div>
+		<div class="site-social">
+
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
-
-<?php wp_footer(); ?>
-
 </body>
 </html>
